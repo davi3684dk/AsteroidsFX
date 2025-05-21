c7bca177551a4099b2a73957d648447b5f54424a
David H <davidhulstroem@gmail.com>
2025-05-21 11:48:27 +0200

    remove comment

--- Core/src/main/java/dk/sdu/cbse/core/Main.java
+++ Core/src/main/java/dk/sdu/cbse/core/Main.java
@@ -3,13 +3,8 @@ package dk.sdu.cbse.core;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
 public class Main extends Application
 {
-
     public static void main( String[] args )
     {
         launch(Main.class);
